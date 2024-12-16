824024cef5c053a6d9fcb55aa0789dd755505fcc
Strong-Fist <jmg8860@naver.com>
2024-12-16 18:51:58 +0900

    동물 기본적인 틀 완성

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Vector;
+
+public class Aminal {
+    private Vector<Image> aminal = new Vector<Image>();
+    public Aminal(int n, String name){
+        for(int i=0;i<n;i++){
+            String path = "image/"+name+"/"+name+i+".png";
+            ImageIcon icon = new ImageIcon(path);
+            Image image = icon.getImage();
+            animal
+        }
+    }
+}
